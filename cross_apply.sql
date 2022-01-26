630c525f307e40ecb17c56c4f3c6206019c8f5bb
Brent Postlethweight <brentpostle@gmail.com>
2022-01-26 11:47:47 -0500

    Update cross_apply.sql

--- cross_apply.sql
+++ cross_apply.sql
@@ -8,7 +8,6 @@ SELECT
 	[athena_to_s3_scholar_data.csv].highestrankschoolname,
 	[athena_to_s3_scholar_data.csv].applyingtoschoolany,
 	[athena_to_s3_scholar_data.csv].adminentered,
-	[athena_to_s3_scholar_data.csv].highestrankschoolname,
 	[athena_to_s3_scholar_data.csv].responsibleadultid,
 	[athena_to_s3_responsible_adults.csv].firstname AS parentfirstname,
 	[athena_to_s3_responsible_adults.csv].lastname AS parentlastname,
@@ -21,10 +20,7 @@ SELECT
 	[athena_to_s3_responsible_adults.csv].borough,
 	[athena_to_s3_responsible_adults.csv].schooldistrict,
 	[athena_to_s3_responsible_adults.csv].zoneerror,
-	[athena_to_s3_customfields_data.csv]."address status"
 FROM [athena_to_s3_scholar_data.csv]
 LEFT JOIN [athena_to_s3_responsible_adults.csv] 
 ON [athena_to_s3_scholar_data.csv].responsibleadultid = [athena_to_s3_responsible_adults.csv].responsibleadultid
-LEFT JOIN [athena_to_s3_customfields_data.csv]
-ON [athena_to_s3_scholar_data.csv].childid = [athena_to_s3_customfields_data.csv].childid
 ORDER BY [athena_to_s3_scholar_data.csv].appdate DESC;
