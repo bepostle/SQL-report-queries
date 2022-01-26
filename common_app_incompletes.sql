d9ca7894abaa46d4602d0efe6c440d5f0b1406cd
Brent Postlethweight <brentpostle@gmail.com>
2022-01-26 10:02:40 -0500

    Update common_app_incompletes.sql

--- common_app_incompletes.sql
+++ common_app_incompletes.sql
@@ -1,11 +1,11 @@
 SELECT 
 	[athena_to_s3_incomplete_scholars.csv].datecreated,
-	athena_to_s3_incomplete_scholars.csv].firstname,
-	athena_to_s3_incomplete_scholars.csv].gradeabbrev,
-	athena_to_s3_incomplete_scholars.csv].responsibleadultfirstname,
-	athena_to_s3_incomplete_scholars.csv].responsibleadultgeneratedpassword,
-	athena_to_s3_incomplete_scholars.csv].responsibleadultemail,
-	athena_to_s3_incomplete_scholars.csv].source 
+	[athena_to_s3_incomplete_scholars.csv].firstname,
+	[athena_to_s3_incomplete_scholars.csv].gradeabbrev,
+	[athena_to_s3_incomplete_scholars.csv].responsibleadultfirstname,
+	[athena_to_s3_incomplete_scholars.csv].responsibleadultgeneratedpassword,
+	[athena_to_s3_incomplete_scholars.csv].responsibleadultemail,
+	[athena_to_s3_incomplete_scholars.csv].source, 
 FROM [athena_to_s3_incomplete_scholars.csv] 
 WHERE source = 'NYC Common App' AND responsibleadultgeneratedpassword != '' 
 ORDER BY datecreated DESC;
