e08a851782ae26a03e439ae078affcdbd15ff884
Brent Postlethweight <brentpostle@gmail.com>
2022-03-23 15:05:27 -0400

    Update incomplete_applications.sql

--- incomplete_applications.sql
+++ incomplete_applications.sql
@@ -4,8 +4,8 @@ SELECT
 	[athena_to_s3_incomplete_scholars.csv].responsibleadultfirstname,
 	[athena_to_s3_incomplete_scholars.csv].source,
 	[athena_to_s3_responsible_adults.csv].zip
-FROM [athena_to_s3_incomplete_scholars.csv]
-LEFT JOIN [athena_to_s3_responsible_adults.csv]
-ON [athena_to_s3_incomplete_scholars.csv].responsibleadultid = [athena_to_s3_responsible_adults.csv].responsibleadultid
+FROM [CSV1].[athena_to_s3_incomplete_scholars.csv]
+LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv]
+ON [CSV1].[athena_to_s3_incomplete_scholars.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
 WHERE [athena_to_s3_incomplete_scholars.csv].source != 'NYC Common App'
 ORDER BY [athena_to_s3_incomplete_scholars.csv].datecreated DESC;
