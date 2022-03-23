542662457a9c8e5855dbd24b1e5957401c9749e6
Brent Postlethweight <brentpostle@gmail.com>
2022-03-23 14:58:16 -0400

    Update all_applied_incl_utms.sql

--- all_applied_incl_utms.sql
+++ all_applied_incl_utms.sql
@@ -8,7 +8,7 @@ SELECT
 	[athena_to_s3_scholar_data.csv].utm_term,
 	[athena_to_s3_scholar_data.csv].utm_medium,
 	[athena_to_s3_scholar_data.csv].utm_content,
-FROM [athena_to_s3_scholar_data.csv]
-LEFT JOIN [athena_to_s3_responsible_adults.csv] 
-ON [athena_to_s3_scholar_data.csv].responsibleadultid = [athena_to_s3_responsible_adults.csv].responsibleadultid
+FROM [CSV1].[athena_to_s3_scholar_data.csv]
+LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] 
+ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
 ORDER BY [athena_to_s3_scholar_data.csv].appdate DESC;
