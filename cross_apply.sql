6d8ef48f736fc9d5c1c36974151d6c6023b7c908
Brent Postlethweight <brentpostle@gmail.com>
2022-03-25 12:53:31 -0400

    Update cross_apply.sql

--- cross_apply.sql
+++ cross_apply.sql
@@ -21,6 +21,7 @@ SELECT
 	[athena_to_s3_responsible_adults.csv].borough,
 	[athena_to_s3_responsible_adults.csv].schooldistrict,
 	[athena_to_s3_responsible_adults.csv].zoneerror,
+	[athena_to_s3_scholar_data.csv].gradeabbrev,
 FROM [CSV1].[athena_to_s3_scholar_data.csv]
 LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] 
 ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
