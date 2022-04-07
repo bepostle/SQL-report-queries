4cfb567b7cbe25c1759d04c5c4387da0f8aeb2e7
Brent Postlethweight <brentpostle@gmail.com>
2022-04-07 13:26:51 -0400

    Update scholar_recruitment_report.sql

--- scholar_recruitment_report.sql
+++ scholar_recruitment_report.sql
@@ -7,10 +7,9 @@ SELECT
 	[bestcurrentstatus],
 	[source],
 FROM [CSV1].[athena_to_s3_scholar_data.csv]
-LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
-WHERE [athena_to_s3_scholar_data.csv].applicationstatus != 'Closed - Duplicate' AND
-[athena_to_s3_scholar_data.csv].applicationstatus != 'Closed - Ineligible' AND 
-[athena_to_s3_scholar_data.csv].applicationstatus != 'Closed - Test App' AND 
-[athena_to_s3_scholar_data.csv].applicationstatus != 'Closed - No Proof' AND
-[athena_to_s3_scholar_data.csv].applicationstatus != 'Closed - Mistakenly Applied'
-ORDER BY [athena_to_s3_scholar_data.csv].appdate;
+WHERE [applicationstatus] != 'Closed - Duplicate' AND
+[applicationstatus] != 'Closed - Ineligible' AND 
+[applicationstatus] != 'Closed - Test App' AND 
+[applicationstatus] != 'Closed - No Proof' AND
+[applicationstatus] != 'Closed - Mistakenly Applied'
+ORDER BY [appdate];
