fffe68d0779dfe13cb5d1ba426261787ad9d6a9d
Brent Postlethweight <brentpostle@gmail.com>
2022-04-25 10:52:05 -0400

    Update all_applied.sql

--- all_applied.sql
+++ all_applied.sql
@@ -1,3 +1,9 @@
 SELECT
-	[email]
-FROM `athena_to_s3_responsible_adults.csv`;
+	[athena_to_s3_scholar_data.csv].responsibleadultid,
+	[email],
+	[appnum],
+	[applicationstatus],
+FROM [CSV1].[athena_to_s3_scholar_data.csv]
+LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv]
+ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
+ORDER BY [appnum] DESC;
