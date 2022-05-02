71ce147415d889815cef6ca5fe39c1044595c3ad
Brent Postlethweight <brentpostle@gmail.com>
2022-05-02 16:33:04 -0400

    Update uniform_promo_code.sql
    removing where clause per FT request

--- uniform_promo_code.sql
+++ uniform_promo_code.sql
@@ -5,6 +5,6 @@ SELECT
 	[currentaccepteddate],
 	[firstacceptdate],
 FROM [CSV1].[athena_to_s3_scholar_data.csv]
-LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
-WHERE [applicationstatus] = 'Accepted'
+LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] 
+ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
 ORDER BY [appnum] DESC;
