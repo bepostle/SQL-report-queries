8768872987256a4c01f0e785be47ef8f3361ee65
Brent Postlethweight <brentpostle@gmail.com>
2022-05-05 12:30:04 -0400

    Update app_volume_report.sql

--- app_volume_report.sql
+++ app_volume_report.sql
@@ -18,6 +18,7 @@ SELECT
 	[athena_to_s3_responsible_adults.csv].state,
 	[athena_to_s3_responsible_adults.csv].schooldistrict,
 	[athena_to_s3_scholar_data.csv].referralsource,
+	[athena_to_s3_scholar_data.csv].acceptedschoolcode,
 FROM [CSV1].[athena_to_s3_scholar_data.csv]
 LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
 WHERE [athena_to_s3_scholar_data.csv].applicationstatus != 'Closed - Duplicate' AND
