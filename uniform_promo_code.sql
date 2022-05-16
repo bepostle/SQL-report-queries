f970a8ac466a97cda7a4b52dcda7c60cf8bb4479
Brent Postlethweight <brentpostle@gmail.com>
2022-05-16 15:29:58 -0400

    Update uniform_promo_code.sql

--- uniform_promo_code.sql
+++ uniform_promo_code.sql
@@ -5,11 +5,12 @@ SELECT
 	[currentaccepteddate],
 	[firstacceptdate],
 	[gender],
-	[gradeabbrev],
-	[athena_to_s3_responsible_adults.csv].[firstname],
-	[athena_to_s3_responsible_adults.csv].[lastname],
-	[email],
-	[email2],
+	[gradeabbrev] AS "Grade",
+	[athena_to_s3_responsible_adults.csv].[firstname] AS "Guardian_First_Name",
+	[athena_to_s3_responsible_adults.csv].[lastname] AS "Guardian_Last_Name",
+	[email] AS "Guardian_Primary_Email",
+	[email] AS "Guardian_Email1",
+	[email2] AS "Guardian_Email2",
 FROM [CSV1].[athena_to_s3_scholar_data.csv]
 LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] 
 ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
