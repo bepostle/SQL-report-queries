a4fc91ac29f65f90d180e6075bfb368d382e4d21
Brent Postlethweight <brentpostle@gmail.com>
2022-05-17 16:18:09 -0400

    Update all_applied_incl_utms.sql

--- all_applied_incl_utms.sql
+++ all_applied_incl_utms.sql
@@ -1,13 +1,15 @@
 SELECT 
-	[athena_to_s3_scholar_data.csv].appdate,
-	[athena_to_s3_scholar_data.csv].childid,
+	appdate,
+	appnum,
+	childid,
+	[athena_to_s3_scholar_data.csv].responsibleadultid,
 	[athena_to_s3_responsible_adults.csv].email,
-	[athena_to_s3_scholar_data.csv].applicationstatus,
-	[athena_to_s3_scholar_data.csv].utm_source,
-	[athena_to_s3_scholar_data.csv].utm_campaign,
-	[athena_to_s3_scholar_data.csv].utm_term,
-	[athena_to_s3_scholar_data.csv].utm_medium,
-	[athena_to_s3_scholar_data.csv].utm_content,
+	applicationstatus,
+	utm_source,
+	utm_campaign,
+	utm_term,
+	utm_medium,
+	utm_content,
 FROM [CSV1].[athena_to_s3_scholar_data.csv]
 LEFT JOIN [CSV1].[athena_to_s3_responsible_adults.csv] 
 ON [CSV1].[athena_to_s3_scholar_data.csv].responsibleadultid = [CSV1].[athena_to_s3_responsible_adults.csv].responsibleadultid
